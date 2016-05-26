1c71085c4a33244ade7b869bdf7b7bd7dd12116d
witzawitz <witzawitz@gmail.com>
2016-05-26 22:01:38 +0300

    File controller with views.

---
+++ controllers/FileController.php
@@ -0,0 +1,17 @@
+<?php
+	namespace vps\uploader\controllers;
+
+	class FileController extends BaseController
+	{
+		public function actionAdd ()
+		{
+		}
+
+		public function actionIndex ()
+		{
+		}
+
+		public function actionView ()
+		{
+		}
+	}
