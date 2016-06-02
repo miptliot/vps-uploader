c8f6bba4c0c53d44f89d471245cd5bc416ea7c0f
witzawitz <witzawitz@gmail.com>
2016-06-02 15:31:09 +0300

    Index page.

--- controllers/UploaderController.php
+++ controllers/UploaderController.php
@@ -2,11 +2,14 @@
 	namespace vps\uploader\controllers;
 
 	use Yii;
+	use vps\uploader\models\File;
 
 	class UploaderController extends BaseController
 	{
 		public function actionIndex ()
 		{
 			$this->title = Yii::t('vps-uploader', 'VPS uploader');
+
+			$this->data('last', File::find()->orderBy([ 'dt' => SORT_DESC ])->all());
 		}
 	}
